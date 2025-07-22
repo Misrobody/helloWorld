c35e4b9f8048ef1ddd2cc741f7a87016af590d37
Misrobody <daphne.larrivain@ecole.ensicaen.fr>
2025-07-22 11:56:53 +0200

    Update c.py

--- helloWorld/c.py
+++ helloWorld/c.py
@@ -1,9 +1,5 @@
-from otkt.tools.instrument import instrument
-from otkt.instrument import instrument
 from time import sleep
 
-@instrument
-@instrument
 def func_c():
     sleep(1)
     print("func_c")
